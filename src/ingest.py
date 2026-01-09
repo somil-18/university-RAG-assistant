73781b7eb7e51c460c7f36e20f4644ee4eda38bd
Somil Dogra <151395938+somil-18@users.noreply.github.com>
2026-01-09 22:24:10 +0530

    improved structure of JSON

--- src/ingest.py
+++ src/ingest.py
@@ -1,54 +1,68 @@
 from llama_parse import LlamaParse
-
 from dotenv import load_dotenv
-import os
+import os, glob, json
 
-import glob
-import json 
 
 load_dotenv()
 
-# path of the PDFs
-path = glob.glob('data/*.pdf')
-json_docs = []
 
-# llama parser to parse the PDFs
+# llamaparser
 parser = LlamaParse(
-    api_key=os.getenv('LLAMA_CLOUD_API_KEY'),
-    result_type='markdown',
+    api_key=os.getenv("LLAMA_CLOUD_API_KEY"),
+    result_type="markdown",
     verbose=True
 )
+
+
+# path of all PDFs
+pdf_files = glob.glob("data/*.pdf")
+
+
+# structure for JSON
+output = {
+    "texts": [],
+    "tables": []
+}
+
+
+# function to check for tables
+def is_table(text):
+    return text.count("|") > 10
+
 
 '''
 - processing files one by one because
 - when I tried loading all files together earlier, the metadata came out empty  
 - so manually giving the file name in metadata
 '''
-for file_path in path:
-    file_name = os.path.basename(file_path) # for e.g. get "rules.pdf"
-    print(f"Parsing: {file_name}...")
-    
-    # we will parse just this one file
-    single_file_docs = parser.load_data(file_path)
-    
-    for doc in single_file_docs:
-        if not doc.metadata:
-            doc.metadata = {}
-            
-        doc.metadata["source"] = file_name # here metadata --> source --> file name
-        
-        json_docs.append({
-            "text": doc.text,
-            "metadata": doc.metadata, 
-            "id_": doc.id_
-        })
+for file_path in pdf_files:
+    file_name = os.path.basename(file_path)
+    print(f"Parsing {file_name}")
 
-# define the output filename
-output_filename = "parsed_data.json"
+    docs = parser.load_data(file_path)
 
-# write to a JSON file
-with open(output_filename, 'w', encoding='utf-8') as f:
-    json.dump(json_docs, f, indent=4)
+    for doc in docs:
+        content = doc.text.strip()
 
-print(f"Successfully saved {len(json_docs)} documents to {output_filename}")
+        if not content:
+            continue
+
+        if is_table(content):
+            output["tables"].append({
+                "content": content,
+                "source": file_name
+            })
+        else:
+            output["texts"].append({
+                "content": content,
+                "source": file_name
+            })
+
+
+# writing JSON
+with open("parsed_data.json", "w", encoding="utf-8") as f:
+    json.dump(output, f, indent=2)
+
+
+print("Done")
 
